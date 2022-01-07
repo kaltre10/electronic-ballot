eb63a2e4d2da790087f9099880a813bd761770c8
Jason Hernandez <kaltre10@gmail.com>
2022-01-06 23:19:27 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import Menu from './components/Menu';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        <Menu />
+        <h1>Boleta Electrónica</h1>
+      </div>
+      <div className="container">
+        <div className="ticket">
+          <form>
+
+          </form>
+        </div>
+        <div className="calculate"></div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Menu.js
@@ -0,0 +1,11 @@
+const Menu = () => {
+    return (
+        <div className='menu'>
+            <div className='bar-menu'></div>
+            <div className='bar-menu'></div>
+            <div className='bar-menu'></div>
+        </div>
+    )
+}
+
+export default Menu;
